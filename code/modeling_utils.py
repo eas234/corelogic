d198902c9cee508476378f8f80cf37886501d27b
eas234 <evelynsmitha@gmail.com>
2025-05-07 15:26:20 -0500

    Update modeling_utils.py
    minor edit to train function to adjust outdir to full path, including filename

--- code/modeling_utils.py
+++ code/modeling_utils.py
@@ -196,7 +196,7 @@ def tune_model(X_train,
                 
     return df
 
-def rf_train_test_write(X_train, X_test, y_train, y_test, meta_train, meta_test, params_path='params_path.pkl', outdir='outdir'):
+def rf_train_test_write(X_train, X_test, y_train, y_test, meta_train, meta_test, params_path='params_path.pkl', out_path='out_path.csv'):
 
     """
     Train model using optimal hyperparams
@@ -215,6 +215,6 @@ def rf_train_test_write(X_train, X_test, y_train, y_test, meta_train, meta_test,
     results_df['y_true'] = y_test.reset_index(drop=True)
     results_df['y_pred'] = y_pred
 
-    results_df.to_csv(os.path.join(outdir,"predictions.csv"), index=False)
+    results_df.to_csv(out_path), index=False)
 
     return results_df
