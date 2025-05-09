96bac0cd408c528aac7655835327976b51c12757
eas234 <evelynsmitha@gmail.com>
2025-05-09 17:39:29 -0500

    Update preprocessor.py
    debug drop funcs

--- code/preprocessor.py
+++ code/preprocessor.py
@@ -257,8 +257,10 @@ class Preprocess:
             self._data = self._data.drop(columns=drop_cols)
             self._data = self._data.reset_index(drop=True)
             self.logger.info("Updating cols attributes to reflect drops")
-            for attr in [self._continuous_cols, self._binary_cols, self._categorical_cols]:
-                attr = [x for x in attr if x in self._data.columns]
+            self._continuous_cols = [x for x in self._continuous_cols if x in self._data.columns]
+            self._binary_cols = [x for x in self._binary_cols if x in self._data.columns]
+            self._categorical_cols = [x for x in self._categorical_cols if x in self._data.columns]
+            
         else:
             processed_data = self._data.copy()
             processed_data = processed_data.drop(columns=drop_cols)
@@ -290,8 +292,9 @@ class Preprocess:
             self._data.drop(columns=drop_cols, inplace=True)
             self._data.reset_index(drop=True, inplace=True)
             self.logger.info("Updating cols attributes to reflect drops")
-            for attr in [self._continuous_cols, self._binary_cols, self._categorical_cols]:
-                attr = [x for x in attr if x in self._data.columns]
+            self._continuous_cols = [x for x in self._continuous_cols if x in self._data.columns]
+            self._binary_cols = [x for x in self._binary_cols if x in self._data.columns]
+            self._categorical_cols = [x for x in self._categorical_cols if x in self._data.columns]
 
         else:
             processed_data = self._data.copy()
