72dc4f207529df66a2a4396354e9540705f6a12b
eas234 <evelynsmitha@gmail.com>
2025-06-02 10:55:18 -0500

    Create clean_census.py
    script to clean/merge census data to corelogic

---
+++ code/cleaning/clean_census.py
@@ -0,0 +1,74 @@
+import os
+import pandas as pd
+import sys
+import yaml
+
+# change working directory to this script's location
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+# append working directory to path
+sys.path.append(os.getcwd())
+from utils import *
+
+# load config
+with open('../../config/clean_data/census_config.yaml', 'r') as stream:
+    out = yaml.safe_load(stream)
+
+# check if tract file already exists. if not, create it.
+if os.path.exists(os.path.join(out['outdir'],'census_tract_level.csv')):
+    tract = pd.read_csv(os.path.join(out['outdir'],'census_tract_level.csv'))
+
+else: 
+    # merge tract data
+    tract = None
+
+    for key in out['tract'].keys():
+        data = pd.read_csv(os.path.join(out['datapath'],out['tract'][key]['filename']))
+        data = preproc_census_df(data, geo='tract', col_list=out['tract'][key]['varlist'])
+        if tract is None:
+            tract = data
+        else:
+            tract = tract.merge(data, how='outer', on='tract_id', validate='1:1')
+
+        # generate varbs
+        tract = gen_varbs(tract, geo='tract')
+
+        # write clean file
+        tract.to_csv(os.path.join(out['outdir'], 'census_tract_level.csv'), index=False)
+
+# check if block group file already exists. if not, create it.
+
+if os.path.exists(os.path.join(out['outdir'],'census_bg_level.csv')):
+    block_group = pd.read_csv(os.path.join(out['outdir'],'census_bg_level.csv'))
+
+else:
+    block_group = None
+
+    for key in out['block_group'].keys():
+        data = pd.read_csv(os.path.join(out['datapath'],out['block_group'][key]['filename']))
+        data = preproc_census_df(data, geo='bg', col_list=out['block_group'][key]['varlist'])
+        if block_group is None:
+            block_group = data
+        else:
+            block_group = block_group.merge(data, how='outer', on='block_group_id', validate='1:1')
+
+        # generate varbs
+        block_group = gen_varbs(block_group, geo='bg')
+
+        # write clean file
+        block_group.to_csv(os.path.join(out['outdir'], 'census_bg_level.csv'), index=False)
+
+# merge to corelogic
+corelogic = pd.read_csv(out['corelogic']['filename'])
+merged = merge_corelogic_census(corelogic, tract, block_group)
+
+# write
+merged.to_csv(os.path.join(out['outdir'], 'corelogic_census_2023.csv'), index=False)
+
+# query census to reverse geocode observations where census merge failed
+output = wrapper(merged, tract, block_group, mapping_path=out['mapping_path'], chunk_size=100)
+
+# write
+output.to_csv(os.path.join(out['outdir'], 'corelogic_census_2023.csv'), index=False)
