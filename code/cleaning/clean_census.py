dd826f49f38c4d236daf1df30408052b89697f70
eas234 <evelynsmitha@gmail.com>
2025-07-07 13:01:50 -0500

    Update clean_census.py
    updating so that corelogic data is pulled in directly from redivis

--- code/cleaning/clean_census.py
+++ code/cleaning/clean_census.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import redivis
 import sys
 import yaml
 
@@ -63,7 +64,11 @@ else:
         block_group.to_csv(os.path.join(out['outdir'], 'census_bg_level.csv'), index=False)
 
 # merge to corelogic
-corelogic = pd.read_csv(out['corelogic']['filename'])
+#corelogic = pd.read_csv(out['corelogic']['filename'])
+user = redivis.user(out['corelogic']['redivis_user'])
+workflow = user.workflow(out['corelogic']['workflow'])
+table = workflow.table(out['corelogic']['workflow_table'])
+corelogic = table.to_pandas_dataframe()
 merged = merge_corelogic_census(corelogic, tract, block_group)
 
 # query census to reverse geocode observations where census merge failed
