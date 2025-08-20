d3ca490851a2686e6e974c8b26ba0c0829f51033
eas234 <evelynsmitha@gmail.com>
2025-08-20 17:51:47 -0500

    Update lasso_loop.py

--- code/models/lasso_loop.py
+++ code/models/lasso_loop.py
@@ -207,7 +207,7 @@ for fips in fips_list:
             write_output=False)
     
     # write
-    results = pd.concat(results, output)
+    results = pd.concat([results, output])
     results.to_csv(results_path)
 
     # delete temporary paths created during model tuning
